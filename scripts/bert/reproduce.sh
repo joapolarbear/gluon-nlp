7cbf013960d672fcccf2df73f9430d89feab3890
Hanpeng Hu <joapolarbear@gmail.com>
2019-11-27 18:35:03 +0800

    Fix the DMLC_ROLE error

--- scripts/bert/reproduce.sh
+++ scripts/bert/reproduce.sh
@@ -13,8 +13,8 @@ export USE_CUDA_PATH=/usr/local/cuda:/usr/local/cudnn/lib64 \
 export https_proxy=http://10.8.77.222:8118 http_proxy=http://10.8.77.222:8118
 
 # ----------------- re-install byteps -----------------
+export DMLC_ROLE="${DMLC_ROLE:-worker}"
 if [ "$1" = "yes" ]; then
-	export DMLC_ROLE="${DMLC_ROLE:-worker}"
 	if [ $DMLC_ROLE = "worker" ]; then
 	update-alternatives --install /usr/bin/gcc gcc $(readlink -f $(which gcc)) 100 
 	update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc x86_64-linux-gnu-gcc $(readlink -f $(which gcc)) 100 
@@ -55,6 +55,7 @@ fi
 cd /root
 
 if [ "$1" = "yes" ]; then
+	if [ $DMLC_ROLE = "worker" ]; then
 	# Set yes to reinstall gluon-nlp and zip, and download dataset.
 	rm -rf gluon-nlp
 	git clone -b test-byteprofile_latency https://github.com/joapolarbear/gluon-nlp.git
@@ -65,6 +66,9 @@ if [ "$1" = "yes" ]; then
 	cd /root/.mxnet/models 
 	wget https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/gluon/dataset/vocab/book_corpus_wiki_en_uncased-a6607397.zip
 	unzip -o *.zip
+	else
+		echo "No need to re-install gluon-nlp."
+	fi
 fi
 
 DATA="/tmp/wiki_en_uncased_data/wiki_en_uncased_0*"
@@ -99,7 +103,6 @@ export WARMUP_RATIO="${WARMUP_RATIO:-0.003125}"
 export NVIDIA_VISIBLE_DEVICES="${GPUS:-0,1,2,3,4,5,6,7}"
 export DMLC_WORKER_ID="${DMLC_WORKER_ID:-0}"
 export DMLC_NUM_WORKER="${DMLC_NUM_WORKER:-1}"
-export DMLC_ROLE=worker
 export NCCL_MIN_NRINGS="${NCCL_MIN_NRINGS:-16}"
 export MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_FWD="${MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_FWD:-120}"
 export MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_BWD="${MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_BWD:-120}"
@@ -130,7 +133,7 @@ python3 /usr/local/byteps/launcher/launch.py \
 		--max_predictions_per_seq $MAX_PREDICTIONS_PER_SEQ \
 		--num_data_workers 4 \
 		--no_compute_acc \
-		--comm_backend device --log_interval $LOGINTERVAL \
+		--log_interval $LOGINTERVAL \
 		--total_batch_size $TOTAL_BATCH_SIZE \
 		--total_batch_size_eval $TOTAL_BATCH_SIZE \
 		--gpus $NVIDIA_VISIBLE_DEVICES \
